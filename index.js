07bb1ad5c444b0a163a6f7c99a64981bf5e82d69
lcysgsg <249313573@qq.com>
2020-08-03 09:36:53 +0800

    1. 使用 prettier 帮助排版 2. 调整了生成文件的结构，不再区分模块，全都统一到 yapi 下

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require("fs-extra")
 const axios = require("axios")
 const path = require("path")
+const prettier = require("prettier")
 
 /**
  * Find the closest package.json file, starting at process.cwd (by default),
@@ -30,11 +31,13 @@ function findPackageJson (startDir) {
  * @returns {Object}                      A object from package.json
  */
 function getPkgJson (opt) {
-  const pkgJson = (opt) ? findPackageJson(opt.startDir) : findPackageJson()
+  const pkgJson = opt ? findPackageJson(opt.startDir) : findPackageJson()
   let fileJson
 
   if (!pkgJson) {
-    throw new Error("Could not find a package.json file. Run 'npm init' to create one.")
+    throw new Error(
+      "Could not find a package.json file. Run 'npm init' to create one."
+    )
   }
 
   try {
@@ -51,14 +54,17 @@ function getPkgJson (opt) {
   }
 
   if (!fileJson.yapi) {
-    throw new Error("Could not find field `yapi` in package.json file. Check it.")
+    throw new Error(
+      "Could not find field `yapi` in package.json file. Check it."
+    )
   }
 
   return fileJson
 }
 
 const pkgJson = getPkgJson()
-const outputDir = pkgJson.yapi.outputDir && path.resolve(process.cwd(), pkgJson.yapi.outputDir)
+const outputDir =
+  pkgJson.yapi.outputDir && path.resolve(process.cwd(), pkgJson.yapi.outputDir)
 
 const OPTIONS = {
   // host 是 yapi 的地址
@@ -72,6 +78,12 @@ const OPTIONS = {
   fileName: pkgJson.yapi.fileName || "apiConfig",
 }
 
+// const data = require("./mock/listMenu.json")
+// fs.writeFile(
+//   `${OPTIONS.outputDir}/${OPTIONS.fileName}.js`,
+//   prettier.format(generateJsApiContent(data.data), { parser: "babel" })
+// )
+
 handleSrouce()
 
 /**
@@ -80,17 +92,21 @@ handleSrouce()
  */
 function handleSrouce () {
   // console.log(`┗|｀O′|┛ 先清一波旧数据 ${OPTIONS.outputDir}`)
-  console.log("┗|｀O′|┛ 程序启动~")
-  console.log(`┗|｀O′|┛ 正在链接目的地 ${OPTIONS.host}${OPTIONS.path}`)
-  axios.get(`${OPTIONS.host}${OPTIONS.path}?token=${OPTIONS.token}`).then(res => {
-    console.log("┗|｀O′|┛ 链接成功")
-    // 可参考 ./listMenu.json
-    const result = res.data
-    const source = result.data
-    console.log(`┗|｀O′|┛ 开始写入 ${OPTIONS.outputDir}`)
-    fs.writeFile(`${OPTIONS.outputDir}/${OPTIONS.fileName}.js`, generateJsApiContent(source))
-    console.log(`┗|｀O′|┛ 对 ${OPTIONS.outputDir}/${OPTIONS.fileName}.js 数据写入完成！`)
-  })
+  console.log(`┗|｀O′|┛  ${OPTIONS.host}${OPTIONS.path}?token=${OPTIONS.token}`)
+  axios
+    .get(`${OPTIONS.host}${OPTIONS.path}?token=${OPTIONS.token}`)
+    .then((res) => {
+      // 可参考 ./listMenu.json
+      const result = res.data
+      const source = result.data
+      fs.writeFile(
+        `${OPTIONS.outputDir}/${OPTIONS.fileName}.js`,
+        prettier.format(generateJsApiContent(source), { parser: "babel" })
+      )
+      console.log(
+        `┗|｀O′|┛  ${path.resolve(OPTIONS.outputDir, OPTIONS.fileName)}.js `
+      )
+    })
 }
 
 /**
@@ -102,43 +118,63 @@ function generateJsApiContent (source) {
   const genrSingleConent = function (list) {
     let result = ""
     list.forEach((item) => {
-      const funcName = reservedWord(getPathWords(item.path, -2))
-      console.log(funcName)
+      // 函数名最少由两个词组成， 上不封顶
+      const funcNameWordNum = item.path.split("/").length <= 4 ? -2 : 2
+      const funcName = reservedWord(getPathWords(item.path, funcNameWordNum))
       result += `
-    // ${item.title}
-    // doc: ${OPTIONS.host}/project/${item.project_id}/interface/api/${item._id}
-    {
-      funcName: "${funcName}",
-      method: "${item.method}",
-      url: "${item.path}",
-    },
-`
+          // ${item.title}
+          // doc: ${OPTIONS.host}/project/${item.project_id}/interface/api/${item._id}
+          {
+            funcName: "${funcName}",
+            method: "${item.method}",
+            url: "${item.path}",
+          },
+      `
     })
     return result
   }
 
-  let fileContent = `/* eslint-disable */
-export default {`
+  const symbolSource = {}
   source.forEach((module) => {
     if (module.list.length === 0) return
-    // "/api/Advise/update" => "Advise"
-    const firstPath = module.list[0].path
-    const fileName = getPathWords(firstPath, "1,2")
-    fileContent += `
-  /**
-   * ==================================================
-   *  ${module.name}
-   *  ${module.desc}
-   * ==================================================
-   */
-  ${fileName}: [`
-    fileContent += genrSingleConent(module.list)
-    fileContent += `  ],
-`
+    module.list.forEach(item => {
+      const firstPath = item.path
+      const fileName = getPathWords(firstPath, "1,2")
+      if (symbolSource[fileName]) {
+        symbolSource[fileName].list.push(item)
+      } else {
+        symbolSource[fileName] = {
+          name: module.name,
+          desc: module.desc,
+          list: [item],
+        }
+      }
+    })
   })
-  fileContent += `}
-`
 
+  let fileContent = `
+      /* eslint-disable */
+        export default {
+          yapi: [
+    `
+  for (const fileName in symbolSource) {
+    const module = symbolSource[fileName]
+
+    if (module.list.length === 0) return
+    // "/api/Advise/update" => "Advise"
+    // const firstPath = module.list[0].path
+    // const fileName = getPathWords(firstPath, "1,2")
+    fileContent += `
+      /**
+       * ==================================================
+       *  ${module.name}
+       *  ${module.desc}
+       * ==================================================
+       */
+      ${genrSingleConent(module.list)}
+    `
+  }
+  fileContent += "]}"
   return fileContent
 }
 
@@ -171,7 +207,7 @@ function getPathWords (path, sliceIdx, divider = "_") {
   let endIndex = arr.length
   if (isNaN(sliceIdx)) {
     // 3,4 => startIndex = 3 , endIndex = 4
-    [startIndex, endIndex] = sliceIdx.split(",")
+    ;[startIndex, endIndex] = sliceIdx.split(",")
   }
   return arr.slice(startIndex, endIndex).join(divider)
 }
