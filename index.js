987870bbdcdc819efbe1c103aab9a843a5ba619b
lcysgsg <249313573@qq.com>
2020-08-26 11:48:44 +0800

    1. 支持 yapi 接口模式多项目导入 2. 重构了项目 3. 现在函数名生成时会识别并排除完整的url了

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const validator = require("validator")
 const fs = require("fs-extra")
 const axios = require("axios")
 const path = require("path")
@@ -90,170 +91,220 @@ function getPkgJson (opt) {
 const pkgJsonDir = findPackageJsonDir()
 const pkgJson = getPkgJson()
 
-const OPTIONS = {
-  // host 是 yapi 的地址
-  host: pkgJson.yapi.host,
-  // 获取新项目接口一般就是改 token，
-  token: pkgJson.yapi.token,
-  path: pkgJson.yapi.path || "/api/interface/list_menu",
+class Main {
+  constructor (options) {
+    this.options = options
+  }
 
-  // 直接使用数据文件。如果有该字段，会直接使用， 不再请求 yapi 接口。
-  // 数据文件地址, 相对于 package.json
-  localFilePath: pkgJson.yapi.localFilePath || null,
-
-  // 相对于 package.json，默认生成到 pkg 同级目录下
-  outputDir: (function () {
-    if (pkgJson.yapi.outputDir) {
-      return path.join(pkgJsonDir, pkgJson.yapi.outputDir)
-    } else {
-      return path.join(pkgJsonDir, "./")
-    }
-  }()),
-  fileName: pkgJson.yapi.fileName || "apiConfig",
-}
-
-handleSrouce()
-
-/**
+  /**
  * 主要处理函数
  * @param {array} source yapi源数据
  */
-function handleSrouce () {
-  // log(`┗|｀O′|┛ 先清一波旧数据 ${OPTIONS.outputDir}`)
-  let url = ""
-  const gen = function (source) {
-    fs.ensureDir(OPTIONS.outputDir, function () {
-      fs.writeFile(
-        `${OPTIONS.outputDir}/${OPTIONS.fileName}.js`,
-        prettier.format(generateJsApiContent(source), { parser: "babel" })
-      )
-      log(`┗|｀O′|┛ 写入 ${path.resolve(OPTIONS.outputDir, OPTIONS.fileName)}.js `)
-    })
-  }
-  if (OPTIONS.localFilePath) {
-    url = path.join(pkgJsonDir, OPTIONS.localFilePath)
-    if (!isFile(url)) {
-      log(chalk.red(`ERR! package.json yapi.localFilePath is not a file. path.resolve: ${url}. `))
-      return false
-    }
-
-    const result = JSON.parse(fs.readFileSync(url, "utf8"))
-    const source = result.data
-    gen(source)
-  } else {
-    url = `${OPTIONS.host}${OPTIONS.path}?token=${OPTIONS.token}`
-    log(`┗|｀O′|┛  ${url}`)
-    axios
-      .get(url)
-      .then((res) => {
-      // 可参考 ./listMenu.json
-        const result = res.data
-        const source = result.data
-        gen(source)
+  handleSrouce () {
+  // log(`┗|｀O′|┛ 先清一波旧数据 ${this.options.outputDir}`)
+    let url = ""
+    const gen = function (source) {
+      fs.ensureDir(this.options.outputDir, () => {
+        fs.writeFile(
+        `${this.options.outputDir}/${this.options.fileName}.js`,
+        prettier.format(this.generateJsApiContent(source), { parser: "babel" })
+        )
+        log(`┗|｀O′|┛ 写入 ${path.resolve(this.options.outputDir, this.options.fileName)}.js `)
       })
-  }
-}
+    }
+    if (this.options.localFilePath) {
+      url = path.join(pkgJsonDir, this.options.localFilePath)
+      if (!isFile(url)) {
+        log(chalk.red(`ERR! package.json yapi.localFilePath is not a file. path.resolve: ${url}. `))
+        process.exit()
+      }
 
-/**
+      const result = JSON.parse(fs.readFileSync(url, "utf8"))
+      const source = result.data
+      gen.call(this, source)
+    } else {
+      url = `${this.options.host}${this.options.path}?token=${this.options.token}`
+      log(`┗|｀O′|┛ ${url}`)
+      axios
+        .get(url)
+        .then((res) => {
+          // 可参考 ./listMenu.json
+          const result = res.data
+          const source = result.data
+          gen.call(this, source)
+        })
+    }
+  }
+
+  /**
  * 拼接内容
  * @param {array} list yapi数组
  */
-function generateJsApiContent (source) {
+  generateJsApiContent (source) {
   // 生成一段
-  const genrSingleConent = function (list) {
-    let result = ""
-    list.forEach((item) => {
+    const genrSingleConent = function (list) {
+      let result = ""
+      list.forEach((item) => {
       // 函数名最少由两个词组成， 上不封顶
       // const funcNameWordNum = item.path.split("/").length <= 3 ? -2 : 2
-      const funcName = reservedWord(getPathWords(item.path, 1))
-      result += `
+        const funcName = this.reservedWord(this.getPathWords(item.path, 1))
+        result += `
           // ${item.title}
-          // doc: ${OPTIONS.host}/project/${item.project_id}/interface/api/${item._id}
+          // doc: ${this.options.host}/project/${item.project_id}/interface/api/${item._id}
           {
             funcName: "${funcName}",
             method: "${item.method}",
             url: "${item.path}",
           },
       `
-    })
-    return result
-  }
+      })
+      return result
+    }
 
-  const symbolSource = {}
-  source.forEach((module) => {
-    if (module.list.length === 0) return
-    module.list.forEach(item => {
-      const firstPath = item.path
-      const fileName = getPathWords(firstPath, "1,2")
-      if (symbolSource[fileName]) {
-        symbolSource[fileName].list.push(item)
-      } else {
-        symbolSource[fileName] = {
-          name: module.name,
-          desc: module.desc,
-          list: [item],
+    const symbolSource = {}
+    source.forEach((module) => {
+      if (module.list.length === 0) return
+      module.list.forEach(item => {
+        const firstPath = item.path
+        const fileName = this.getPathWords(firstPath, "1,2")
+        if (symbolSource[fileName]) {
+          symbolSource[fileName].list.push(item)
+        } else {
+          symbolSource[fileName] = {
+            name: module.name,
+            desc: module.desc,
+            list: [item],
+          }
         }
-      }
+      })
     })
-  })
 
-  let fileContent = `
+    let fileContent = `
       /* eslint-disable */
         export default [
     `
-  for (const fileName in symbolSource) {
-    const module = symbolSource[fileName]
+    for (const fileName in symbolSource) {
+      const module = symbolSource[fileName]
 
-    if (module.list.length === 0) return
-    // "/api/Advise/update" => "Advise"
-    // const firstPath = module.list[0].path
-    // const fileName = getPathWords(firstPath, "1,2")
-    fileContent += `
+      if (module.list.length === 0) return
+      // "/api/Advise/update" => "Advise"
+      // const firstPath = module.list[0].path
+      // const fileName = getPathWords(firstPath, "1,2")
+      fileContent += `
       /**
        * ==================================================
        *  ${module.name}
        *  ${module.desc}
        * ==================================================
        */
-      ${genrSingleConent(module.list)}
+      ${genrSingleConent.call(this, module.list)}
     `
+    }
+    fileContent += "]"
+    return fileContent
   }
-  fileContent += "]"
-  return fileContent
-}
 
-/**
+  /**
  * 防止与保留字冲突
  * @param {string} word 单词
  */
-function reservedWord (word) {
-  switch (word) {
-    case "delete":
-      return "del"
+  reservedWord (word) {
+    switch (word) {
+      case "delete":
+        return "del"
 
-    default:
-      return word
+      default:
+        return word
+    }
   }
-}
 
-/**
+  /**
  * 根据位置取路径上的单词
  *   - getPathWords('/v1/Advise/update', 1) => 'Advise_update'
  * @param {string} path 路径
  * @param {string|number} sliceIdx 同 Array.prototype.splice(index)
  * @param {string} divider 链接函数名的符号
  */
-function getPathWords (path, sliceIdx, divider = "_") {
-  // 统一成方便处理的模样 => v1/Advise/update
-  path = path[0] === "/" ? path.replace("/", "") : path
-  const arr = path.split("/")
-  let startIndex = sliceIdx
-  let endIndex = arr.length
-  if (isNaN(sliceIdx)) {
+  getPathWords (path, sliceIdx, divider = "_") {
+  // 如果带域名，就截取后面的路径
+    if (validator.isURL(path)) {
+      path.replace(/.*\/\/.*?\//, "")
+    }
+
+    // 统一成方便处理的模样 /v1/Advise/update => v1/Advise/update
+    path = path[0] === "/" ? path.replace("/", "") : path
+    // 统一成方便处理的模样 v1/Advise/update/ => v1/Advise/update
+    path = path[path.length - 1] === "/" ? path.replace("/", "") : path
+    const arr = path.split("/")
+    let startIndex = sliceIdx
+    let endIndex = arr.length
+    if (isNaN(sliceIdx)) {
     // 3,4 => startIndex = 3 , endIndex = 4
-    ;[startIndex, endIndex] = sliceIdx.split(",")
+      ;[startIndex, endIndex] = sliceIdx.split(",")
+    }
+    return arr.slice(startIndex, endIndex).join(divider)
   }
-  return arr.slice(startIndex, endIndex).join(divider)
 }
 
-module.exports = handleSrouce
+function run (pkgFieldYapi = pkgJson.yapi) {
+  const yapiOptionsFormat = function (aYapi) {
+    let mode = {}
+    if (aYapi.localFilePath) {
+      mode = {
+        // 直接使用数据文件。如果有该字段，会直接使用， 不再请求 yapi 接口。
+        // 数据文件地址, 相对于 package.json
+        localFilePath: aYapi.localFilePath || null,
+      }
+    } else {
+      mode = {
+        // host 是 yapi 的地址
+        host: (function () {
+          if (!validator.isURL(String(aYapi.host))) {
+            log(chalk.red(`YAPI ERR! package.json invalid host：${aYapi.host}`))
+            log(chalk.red(`${JSON.stringify(aYapi, null, 2)} `))
+            process.exit()
+          }
+          return aYapi.host
+        }()),
+
+        // 获取新项目接口一般就是改 token，
+        token: (function () {
+          if (!aYapi.token) {
+            log(chalk.red(`YAPI ERR! package.json token required：${aYapi.token}`))
+            log(chalk.red(`${JSON.stringify(aYapi, null, 2)} `))
+            process.exit()
+          }
+          return aYapi.token
+        }()),
+
+        path: aYapi.path || "/api/interface/list_menu",
+      }
+    }
+    return {
+      // 相对于 package.json，默认生成到 pkg 同级目录下
+      outputDir: (function () {
+        if (aYapi.outputDir) {
+          return path.join(pkgJsonDir, aYapi.outputDir)
+        } else {
+          return path.join(pkgJsonDir, "./")
+        }
+      }()),
+      fileName: aYapi.fileName || "apiConfig",
+
+      ...mode,
+    }
+  }
+
+  if (Array.isArray(pkgFieldYapi)) {
+    pkgFieldYapi.forEach(yapiOptions => {
+      const r = new Main(yapiOptionsFormat(yapiOptions))
+      r.handleSrouce()
+    })
+  } else {
+    const r = new Main(yapiOptionsFormat(pkgFieldYapi))
+    r.handleSrouce()
+  }
+}
+
+exports.Main = Main
+exports.run = run
